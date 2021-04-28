623e5e872c28cf62b321a1b0b15eac562ca7db2a
TrainingCode <greenwind.01011994@gmail.com>
2021-04-28 10:58:47 +0700

    luu

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const mongoose =require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://windwebdev:phong1994@cluster0.mkuro.mongodb.net/mern-learnit?retryWrites=true&w=majority`, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+
+        console.log('MongoDB connected'); // log ra xem đã kết nối
+    } catch (error) {
+        console.log(err.message);
+        process.exit(1)
+    }
+}
+
+connectDB();//gọi hàm
 
 const app = express();
 
