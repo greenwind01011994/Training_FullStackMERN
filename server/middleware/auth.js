b850b2ec7019d59246efd9d821ffb828e1a37e86
TrainingCode <greenwind.01011994@gmail.com>
2021-04-30 12:46:59 +0700

    luu

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     const authHeader = req.header('Authorization'); //'Authorization' lấy giá trị value header ra
-    const token = authHeader && authHeader.splist(' ')[1]; // && là nêu không có authHeader thì nó sẽ chính là authHeader và nếu có authHeader thì nó là cái đăng sau && authHeader.split('') 
+    const token = authHeader && authHeader.split(' ')[1]; // && là nêu không có authHeader thì nó sẽ chính là authHeader và nếu có authHeader thì nó là cái đăng sau && authHeader.split('') 
 
     if(!token)
     return res.status(401).json({success: false, message: 'Access token not found'});
