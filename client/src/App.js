93f028f113b2faa441c5106598ffae72e69a1c89
TrainingCode <greenwind.01011994@gmail.com>
2021-05-03 14:59:34 +0700

    xong

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,6 @@ function App() {
   return <Router>
     <Switch>
 			<Route exact path='/' component={Landing} />
-				<Route
-					exact
-					path='/login'
-					ender={props => <Auth {...props} authRoute='login' />}
-				/>
-				<Route
-					exact
-					path='/register'
-					render={props => <Auth {...props} authRoute='register' />}
-			  />
-				<ProtectedRoute exact path='/dashboard' component={Dashboard} />
-				<ProtectedRoute exact path='/about' component={About} />
 		</Switch>
   </Router>
 }
